7e82194cc02ac52f7baa085ff4caa6a43dbb71d9
Jachdich <jamesedkitching@gmail.com>
2023-11-14 01:31:10 +0000

    some progress, finally

--- main.hs
+++ main.hs
@@ -2,74 +2,79 @@ import Graphics.Gloss
 import System.Random
 import System.IO.Unsafe
 import Data.Array.IO
+import Graphics.Gloss.Interface.IO.Game
 
-type Cell = (Float, Int, Int)
-type FlowVec = IO (IOArray Int Float)
-type Model = ([Cell], FlowVec, FlowVec)
-
-horizontals :: Model -> FlowVec
-horizontals (cells, horiz, vert) = horiz
-
-verticals :: Model -> FlowVec
-verticals (cells, horiz, vert) = vert
-
-cells :: Model -> [Cell]
-cells (cells, horiz, vert) = cells
-
-withCells :: Model -> [Cell] -> Model
-withCells (_, horiz, vert) cells = (cells, horiz, vert)
-
-withHoriz :: Model -> FlowVec -> Model
-withHoriz (cells, _, vert) horiz = (cells, horiz, vert)
-
-withVert :: Model -> FlowVec -> Model
-withVert (cells, horiz, _) vert = (cells, horiz, vert)
+data Cell = Cell {density :: Float, x :: Int, y :: Int, wall :: Bool, pressure :: Float}
+type FlowVec = (IOArray Int Float)
+data Model = Model {cells :: IOArray Int Cell, horiz :: FlowVec, vert :: FlowVec}
 
 uAt :: Model -> Int -> Int -> IO Float
-uAt model x y = readArray (horizontals model) (y * xCells + x)
+uAt model x y = readArray (horiz model) (y * xCells + x)
 vAt :: Model -> Int -> Int -> IO Float
-vAt model x y = readArray (verticals model) (y * xCells + x)
+vAt model x y = readArray (vert model) (y * xCells + x)
+
+uSet :: Model -> Int -> Int -> Float -> IO ()
+uSet model x y = writeArray (horiz model) (y * xCells + x)
+vSet :: Model -> Int -> Int -> Float -> IO ()
+vSet model x y = writeArray (vert model) (y * xCells + x)
 
 width = 1280
 height = 720
 cellWidth :: Int
 cellHeight :: Int
-cellWidth = 100
-cellHeight = 100
+cellWidth = 10
+cellHeight = 10
 
 g = -9.81 -- downwards
 
 xCells = width `div` cellWidth
 yCells = height `div` cellHeight
 
-initial :: Model
-initial = ([(0.0, x, y) | x <- [1..(xCells)], y <- [1..(yCells)]],
-           newArray (0,(xCells * (yCells + 1))) 0.0,
-           newArray (0,((xCells + 1) * yCells)) 0.0)
-
-main :: IO()
-main = simulate (InWindow "Window" window (0, 0)) black 30 initial draw update
+initial :: IO Model
+initial = do
+        horiz <- newArray (0,(xCells * (yCells + 1))) 0.0
+        vert <- newArray (0,((xCells + 1) * yCells)) 0.0
+        cells <- newArray (0,(xCells * yCells)) (Cell 1.0 0 0 False 0.0)
+        return (cells, horiz, vert)
+        -- return ([(Cell 1.0 x y (isWall x y) 0.0) | x <- [0..(xCells-1)], y <- [0..(yCells-1)]], horiz, vert)
     where
-        window = (width, height)
+        isWall x y = x == 0 || y == 0 || x == xCells - 1 || y == yCells - 1
 
+main :: IO ()
+main = do
+    model <- initial
+    playIO (InWindow "Window" (width, height) (0, 0)) black 30 model draw handleInput update
 
 pos :: Int -> Int -> Picture -> Picture
-pos x y = translate ((fromIntegral x * fromIntegral cellWidth) - (fromIntegral width / 2)) ((fromIntegral y * fromIntegral cellHeight) - (fromIntegral height / 2))
+pos x y = translate ((fromIntegral x * fromIntegral cellWidth) - (fromIntegral width / 2)) (fromIntegral (y * cellHeight) - (fromIntegral height / 2))
+rectangleSolidCorner width height = translate (width / 2) (height / 2) $ rectangleSolid width height
 
-draw :: Model -> Picture
-draw model = Pictures [color (makeColor col col col 1.0) $ pos x y $ rectangleSolid (fromIntegral cellWidth) (fromIntegral cellHeight) | (col, x, y) <- cells model]
+draw :: Model -> IO Picture
+draw model = do
+            putStrLn "he"
+            return (Pictures [color (makeColor (density cell) (density cell) (if wall cell then 1.0 else 0.0) 1.0) $ pos (x cell) (y cell) $ rectangleSolidCorner (fromIntegral cellWidth) (fromIntegral cellHeight) | cell <- cells model])
 
+handleInput :: Event -> Model -> IO Model
+handleInput event model = do return model
 
-update vp dt model = initial
+update :: Float -> Model -> IO Model
+update dt model = divergence dt model
 
-integration dt model = model `withCells` [(value + g * dt, x, y) | (value, x, y) <- cells model]
--- divergence dt model = [
---                         (uAt model x y) + (divergence x y) / 4
---                         (uAt model (x+1) y) - (divergence x y) / 4
---                         (vAt model x y) - (divergence x y) / 4
---                         (vAt model x (y+1) + (divergence x y) / 4
---                          | x <- [0..xCells-1], y <- [0..yCells - 1]]
- 
---     where
---         divergence x y = (uAt model x y) + vAt model x y
---                         -(uAt model (x + 1) y) - vAt model x (y + 1)
+-- integration dt model = model `withCells` [(Cell (value + g * dt) x y wall pressure) | (Cell value x y wall pressure) <- cells model]
+
+divergence :: Float -> Model -> IO Model
+divergence dt model = last 
+                      [(do 
+                        uxy <- uAt model x y
+                        vxy <- vAt model x y
+                        ux1y <- uAt model (x + 1) y
+                        vxy1 <- vAt model x (y + 1)
+                        let diverge = (uxy + vxy - ux1y - vxy1) / 4 * 1.9 -- 1.9 is over relaxation constant
+                        let s = sum $ map fromEnum [isWall (x - 1) y, isWall x (y - 1), isWall (x + 1) y, isWall x (y + 1)]
+                        uSet model x   y     (uxy  + diverge)
+                        uSet model (x+1) y   (ux1y - diverge)
+                        vSet model x   y     (vxy  - diverge)
+                        vSet model x   (y+1) (vxy1 + diverge)
+                        return model) | (Cell value x y wall pressure) <- cells model, not wall]
+            where
+                isWall x y = wall (cells model !! (y * xCells + x))
