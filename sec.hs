bf9150e6719026648fbf397090a4797fd3dfac7b
Jachdich <jamesedkitching@gmail.com>
2023-11-16 18:35:34 +0000

    it broken

--- sec.hs
+++ sec.hs
@@ -27,8 +27,11 @@ cellWidth :: Int
 cellHeight :: Int
 cellWidth = 10
 cellHeight = 10
+cellSpacing = (fromIntegral cellWidth) * 0.01 -- assume 1 pixel = 0.01m
 
 g = -9.81 -- downwards
+dt = 1.0/30.0
+fluidDensity = 1000.0 -- water ig
 
 xCells = width `div` cellWidth
 yCells = height `div` cellHeight
@@ -79,11 +82,13 @@ idle state = do
 drawModel :: Model -> IO ()
 drawModel model = renderPrimitive Quads $ do
             putStrLn "he"
+            update model
             forM_ [0..(xCells*yCells)-1] $ \i -> do
                 cell <- readArray (cells model) i
                 let x = i `mod` xCells
                 let y = i `div` xCells
-                color $ Color3 (density cell) (density cell) (if wall cell then 1.0 else 0.0)
+                color $ Color3 (pressure cell) (pressure cell) (if wall cell then 1.0 else 0.0)
+                print $ pressure cell
                 let screenX = (fromIntegral x / fromIntegral xCells * 2 - 1) :: GLfloat
                 let screenY = (fromIntegral y / fromIntegral yCells * 2 - 1) :: GLfloat
                 -- print screenX
@@ -97,39 +102,58 @@ drawModel model = renderPrimitive Quads $ do
 -- handleInput :: Event -> Model -> IO Model
 -- handleInput event model = do return model
 
-update :: Float -> Model -> IO Model
-update dt model = do 
-        -- putStrLn (show dt)
-        return model --divergence dt model
+update :: Model -> IO Model
+update model = do 
+        -- integrate model
+        mapArray (\(Cell a b pressure) -> Cell a b 0) (cells model)
+        model <- divergence model
+        return model
 
 
 integrate model = do
-    forM_ [0..xCells-1] $ \x -> do
-        forM_ [0..yCells-1] $ \y -> do
-            uSet model  whatever HELAGIJEGHI
+    forM_ [1..xCells-1] $ \x -> do
+        forM_ [1..yCells-1] $ \y -> do
+            (Cell _ wall _) <- readArray (cells model) (y * xCells + x)
+            (Cell _ wall1 _) <- readArray (cells model) (y * xCells + x - 1)
+            v <- vAt model x y
+            if not wall && not wall1 then
+                vSet model x y (v + g * dt)
+            else return ()
 
-divergence :: Float -> Model -> IO Model
-divergence dt model = do 
-                forM_ [0..xCells-1] $ \x -> do
-                    forM_ [0..yCells-1] $ \y -> do
-                        uxy <- uAt model x y
-                        vxy <- vAt model x y
-                        ux1y <- uAt model (x + 1) y
-                        vxy1 <- vAt model x (y + 1)
-                        let diverge = (uxy + vxy - ux1y - vxy1) / 4 * 1.9 -- 1.9 is over relaxation constant
+divergence :: Model -> IO Model
+divergence model = do
+                let coefficientPressure = fluidDensity * cellSpacing / dt
+                forM_ [1..xCells-2] $ \x -> do
+                    forM_ [1..yCells-2] $ \y -> do
                         w1 <- isWall (x - 1) y
                         w2 <- isWall x (y - 1)
                         w3 <- isWall (x + 1) y
                         w4 <- isWall x (y + 1)
-                        let s = sum $ map fromEnum [w1, w2, w3, w4]
-                        uSet model x   y     (uxy  + diverge)
-                        uSet model (x+1) y   (ux1y - diverge)
-                        vSet model x   y     (vxy  - diverge)
-                        vSet model x   (y+1) (vxy1 + diverge)
-                        (Cell value wall pressure) <- readArray (cells model) (y * xCells + x)
+                        let s = sum $ map fromEnum $ map not [w1, w2, w3, w4]
+                        uxy <- uAt model x y
+                        vxy <- vAt model x y
+                        ux1y <- uAt model (x + 1) y
+                        vxy1 <- vAt model x (y + 1)
+                        let diverge = (uxy + vxy - ux1y - vxy1) / 4
+                        cell <- readArray (cells model) (y * xCells + x)
+                        let p = -diverge / (fromIntegral s)-- * 1.9; -- maybe -diverge
+                        let pressureHere = (pressure cell) + p * coefficientPressure
+                        uSet model x   y     (uxy  - (p * fromIntegral $ fromEnum $ not w1))
+                        uSet model (x+1) y   (ux1y + (p * fromIntegral $ fromEnum $ not w3))
+                        vSet model x   y     (vxy  - (p * fromIntegral $ fromEnum $ not w2))
+                        vSet model x   (y+1) (vxy1 + (p * fromIntegral $ fromEnum $ not w4))
+                        writeArray (cells model) (y * xCells + x) (Cell (density cell) (wall cell) pressureHere)
                         return ()
                 return model
             where
                 isWall x y = do
                     (Cell _ wall _) <- readArray (cells model) (y * xCells + x)
                     return wall
+
+-- advection :: Model -> IO Model
+-- advection model = forM_ [1..xCells-2] $ \x -> do
+--                       forM_ [1..yCells-2] $ \y -> do
+--                           let currentPos = ...
+--                           currentVec <- uAt model x y
+--                           let previousPos = ...
+                          
