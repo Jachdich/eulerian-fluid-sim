1866703ec1422aa82607cd141178be5e42c15cdf
Jachdich <jamesedkitching@gmail.com>
2023-11-17 18:24:40 +0000

    start on advection

--- sec.hs
+++ sec.hs
@@ -7,7 +7,12 @@ import Graphics.UI.GLUT
 import Codec.Picture
 import Control.Monad (forM_, forM, replicateM_)
 
-data Model = Model {wall :: IOArray Int Float, horiz :: IOArray Int Float, vert :: IOArray Int Float, pressure :: IOArray Int Float}
+data Model = Model {wall :: IOArray Int Float,
+                    horiz :: IOArray Int Float,
+                    vert :: IOArray Int Float,
+                    pressure :: IOArray Int Float,
+                    newHoriz :: IOArray Int Float,
+                    newVert :: IOArray Int Float}
 
 at :: IOArray Int Float -> Int -> Int -> IO Float
 at vec x y = readArray vec (x * yCells + y)
@@ -27,7 +32,7 @@ cellHeight = 10
 cellSpacing = 0.01
 
 g = -9.81 -- downwards
-dt = 1.0/120.0
+dt = 1.0/60.0
 fluidDensity = 1000.0 -- water ig
 
 xCells = width `div` cellWidth
@@ -37,12 +42,14 @@ initial :: IO Model
 initial = do
         horiz <- newArray (0,(xCells * (yCells + 1))) 0.0
         vert <- newArray (0,((xCells + 1) * yCells)) 0.0
+        newHoriz <- newArray (0,(xCells * (yCells + 1))) 0.0
+        newVert <- newArray (0,((xCells + 1) * yCells)) 0.0
         wall <- newArray (0,(xCells * yCells)) 0.0
         pressure <- newArray (0,(xCells * yCells)) 0.0
         forM_ [0..xCells-1] $ \x -> do
             forM_ [0..yCells-1] $ \y -> do
                 set wall x y (isWall x y)
-        return (Model wall horiz vert pressure)
+        return (Model wall horiz vert pressure newHoriz newVert)
     where
         isWall x y = if (x == 0 || y == 0 || x == xCells - 1 || y == yCells - 1) then 0.0 else 1.0
 
@@ -87,7 +94,7 @@ drawModel model = renderPrimitive Quads $ do
                 let p = map_lol pressure minPressure maxPressure 0.0 1.0
                 color $ Color3 p p (1.0 - wall)
                 v <- readArray (vert model) i
-                print v
+                -- print v
                 let screenX = (fromIntegral x / fromIntegral xCells * 2 - 1) :: GLfloat
                 let screenY = (fromIntegral y / fromIntegral yCells * 2 - 1) :: GLfloat
                 -- print screenX
@@ -110,7 +117,7 @@ update model = do
         return model
 
 integrate :: Model -> IO ()
-integrate Model {wall, horiz, vert, pressure} = do
+integrate Model {wall, horiz, vert, pressure, newHoriz, newVert} = do
     forM_ [1..xCells-1] $ \x -> do
         forM_ [1..yCells-2] $ \y -> do
             wall0 <- at wall x y
@@ -120,8 +127,8 @@ integrate Model {wall, horiz, vert, pressure} = do
             else return ()
 
 divergence :: Model -> IO ()
-divergence Model {wall, horiz, vert, pressure} = 
-    -- replicateM_ 10 $
+divergence Model {wall, horiz, vert, pressure, newHoriz, newVert} = 
+    replicateM_ 10 $
     forM_ [1..xCells-2] $ \x -> do
         forM_ [1..yCells-2] $ \y -> do
             x0 <- at wall (x - 1) y
@@ -144,10 +151,42 @@ divergence Model {wall, horiz, vert, pressure} =
 
     where
         coefficientPressure = fluidDensity * cellSpacing / dt
--- advection :: Model -> IO Model
--- advection model = forM_ [1..xCells-2] $ \x -> do
---                       forM_ [1..yCells-2] $ \y -> do
---                           let currentPos = ...
---                           currentVec <- uAt model x y
---                           let previousPos = ...
-                          
+
+-- vadd :: Num a => (a, a) -> (a, a) -> (a, a)
+-- (x0, y0) `vadd` (x1, y1) = (x0 + x1, y0 + y1)
+
+-- vmul :: Num a => (a, a) -> (a, a) -> (a, a)
+-- (x0, y0) `vmul` (x1, y1) = (x0 * x1, y0 * y1)
+
+-- advection :: Model -> IO ()
+-- advection Model {wall, horiz, vert, pressure, newHoriz, newVert} = do
+--     forM_ [0..xCells * yCells - 1] $ \i -> do
+--         u <- readArray horiz i
+--         v <- readArray vert i
+--         writeArray newHoriz i u
+--         writeArray newVert i v
+    
+--     forM_ [1..xCells-2] $ \x -> do
+--         forM_ [1..yCells-2] $ \y -> do
+--             let currentX = x * cellSpacing
+--             let currentY = y * cellSpacing + (cellSpacing / 2)
+--             currentVec <- velocityAtU model x y
+--             let previousPos = 1
+--             return ()
+--     where
+--         velocityAtU model x y = do
+--             u <- at (horiz model) x y
+--             v01 <- at (vert model) (x-1) (y+1)
+--             v11 <- at (vert model) x (y+1)
+--             v00 <- at (vert model) (x-1) y
+--             v10 <- at (vert model) x y
+--             let v = (v01 + v11 + v00 + v10) / 4
+--             return (u, v)
+--         velocityAtV model x y = do
+--             v <- at (vert model) x y
+--             u0 <- at (horiz model) (x+1) (y-1)
+--             u1 <- at (horiz model) x (y-1)
+--             u2 <- at (horiz model) (x+1) y
+--             u3 <- at (horiz model) x y
+--             let v = (u0 + u1 + u2 + u3) / 4
+--             return (u, v)
