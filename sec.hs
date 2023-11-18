848b223dadec5d8401083cca7d634318c0259f4e
Jachdich <jamesedkitching@gmail.com>
2023-11-18 03:21:43 +0000

    good progress, I think it works

--- sec.hs
+++ sec.hs
@@ -63,7 +63,11 @@ main = do
     createWindow "Test"
     displayCallback $= draw state
     idleCallback $= Just (idle state)
+    motionCallback $= Just (mouse state)
     mainLoop
+
+mouse state event = do
+    print event
 
 draw :: IORef Model -> DisplayCallback
 draw state = do 
@@ -105,16 +109,28 @@ drawModel model = renderPrimitive Quads $ do
                 vertex $ Vertex2 (screenX + dx) (screenY + dy)
                 vertex $ Vertex2 screenX (screenY + dy)
 
--- handleInput :: Event -> Model -> IO Model
--- handleInput event model = do return model
-
 update :: Model -> IO Model
 update model = do 
         integrate model
         forM_ [0..xCells * yCells - 1] $ \i -> do
             writeArray (pressure model) i 0
         divergence model
+
+        extrapolate model -- ???
+        advection model
         return model
+
+extrapolate Model {wall, horiz, vert, pressure, newHoriz, newVert} = do
+    forM_ [0..xCells - 1] $ \x -> do
+        u1 <- at horiz x 1
+        set horiz x 0 u1
+        u2 <- at horiz x (yCells - 2)
+        set horiz x (yCells - 1) u2
+    forM_ [0..yCells - 1] $ \y -> do
+        v1 <- at vert 1 y
+        set vert 0 y v1
+        v2 <- at vert (xCells - 2) y
+        set vert (xCells - 1) y v2
 
 integrate :: Model -> IO ()
 integrate Model {wall, horiz, vert, pressure, newHoriz, newVert} = do
@@ -152,41 +168,94 @@ divergence Model {wall, horiz, vert, pressure, newHoriz, newVert} =
     where
         coefficientPressure = fluidDensity * cellSpacing / dt
 
--- vadd :: Num a => (a, a) -> (a, a) -> (a, a)
--- (x0, y0) `vadd` (x1, y1) = (x0 + x1, y0 + y1)
+vsub :: Num a => (a, a) -> (a, a) -> (a, a)
+(x0, y0) `vsub` (x1, y1) = (x0 - x1, y0 - y1)
 
--- vmul :: Num a => (a, a) -> (a, a) -> (a, a)
--- (x0, y0) `vmul` (x1, y1) = (x0 * x1, y0 * y1)
+vmul :: Num a => (a, a) -> a -> (a, a)
+(x0, y0) `vmul` n = (x0 * n, y0 * n)
 
--- advection :: Model -> IO ()
--- advection Model {wall, horiz, vert, pressure, newHoriz, newVert} = do
---     forM_ [0..xCells * yCells - 1] $ \i -> do
---         u <- readArray horiz i
---         v <- readArray vert i
---         writeArray newHoriz i u
---         writeArray newVert i v
+clamp :: Ord a => a -> a -> a -> a
+clamp a lower upper = if a > upper then upper else (if a < lower then lower else a)
+
+advection :: Model -> IO ()
+advection Model {wall, horiz, vert, pressure, newHoriz, newVert} = do
     
---     forM_ [1..xCells-2] $ \x -> do
---         forM_ [1..yCells-2] $ \y -> do
---             let currentX = x * cellSpacing
---             let currentY = y * cellSpacing + (cellSpacing / 2)
---             currentVec <- velocityAtU model x y
---             let previousPos = 1
---             return ()
---     where
---         velocityAtU model x y = do
---             u <- at (horiz model) x y
---             v01 <- at (vert model) (x-1) (y+1)
---             v11 <- at (vert model) x (y+1)
---             v00 <- at (vert model) (x-1) y
---             v10 <- at (vert model) x y
---             let v = (v01 + v11 + v00 + v10) / 4
---             return (u, v)
---         velocityAtV model x y = do
---             v <- at (vert model) x y
---             u0 <- at (horiz model) (x+1) (y-1)
---             u1 <- at (horiz model) x (y-1)
---             u2 <- at (horiz model) (x+1) y
---             u3 <- at (horiz model) x y
---             let v = (u0 + u1 + u2 + u3) / 4
---             return (u, v)
+    forM_ [1..xCells-1] $ \x -> do
+        forM_ [1..yCells-1] $ \y -> do
+            sxy  <- at wall x y
+            sx1y <- at wall (x - 1) y
+            sxy1 <- at wall x (y - 1)
+            
+            if sxy /= 0.0 && sx1y /= 0.0 && y < yCells - 1 then do
+                let currentPos = ((fromIntegral x) * cellSpacing,
+                                (fromIntegral y) * cellSpacing + (cellSpacing / 2))
+                currentVelocity <- velocityAtU x y
+                let previousPos = currentPos `vsub` (currentVelocity `vmul` dt)
+                u <- uncurry (sample horiz 0.0 (cellSpacing / 2)) previousPos
+                set newHoriz x y u
+            else return ()
+
+            if sxy /= 0.0 && sxy1 /= 0.0 && x < xCells - 1 then do
+                let currentPos = ((fromIntegral x) * cellSpacing + (cellSpacing / 2),
+                                (fromIntegral y) * cellSpacing)
+                currentVelocity <- velocityAtV x y
+                let previousPos = currentPos `vsub` (currentVelocity `vmul` dt)
+                v <- uncurry (sample vert (cellSpacing / 2) 0.0) previousPos
+                set newVert x y v
+            else return ()
+
+    forM_ [0..xCells * yCells - 1] $ \i -> do
+        u <- readArray newHoriz i
+        v <- readArray newVert i
+        writeArray horiz i u
+        writeArray vert i v
+    where
+        velocityAtU x y = do
+            u <- at horiz x y
+            v01 <- at vert (x+1) (y-1)
+            v11 <- at vert x (y-1)
+            v00 <- at vert (x+1) y
+            v10 <- at vert x y
+            let v = (v01 + v11 + v00 + v10) / 4
+            return (u, v)
+        velocityAtV x y = do
+            v <- at vert x y
+            u0 <- at horiz (x-1) (y+1)
+            u1 <- at horiz x (y+1)
+            u2 <- at horiz (x-1) y
+            u3 <- at horiz x y
+            let u = (u0 + u1 + u2 + u3) / 4
+            return (u, v)
+
+        sample field dx dy ix iy = do
+            -- make sure the x and y values arent whack
+            let x = clamp ix cellSpacing ((fromIntegral xCells - 1) * cellSpacing)
+            let y = clamp iy cellSpacing ((fromIntegral yCells - 1) * cellSpacing)
+            
+            -- which cell is the given coordinate inside?
+            let gridX = floor ((x - dx) / cellSpacing)
+            let gridY = floor ((y - dy) / cellSpacing)
+
+            -- Offsets of the position into the current cell
+            let offsetX = (x - dx) / cellSpacing - (fromIntegral gridX)
+            let offsetY = (y - dy) / cellSpacing - (fromIntegral gridY)
+
+            -- Weights of each velocity component
+            let w00 = 1 - offsetX
+            let w01 = offsetX
+            let w10 = 1 - offsetY
+            let w11 = offsetY
+
+            -- sample each velocity component
+            vx0y0 <- at field gridX gridY
+            vx1y0 <- at field (gridX + 1) gridY
+            vx0y1 <- at field gridX (gridY + 1)
+            vx1y1 <- at field (gridX + 1) (gridY + 1)
+
+            -- final weighted sum
+            return ( 
+                w00 * w10 * vx0y0 +
+                w01 * w10 * vx1y0 +
+                w00 * w11 * vx0y1 +
+                w01 * w11 * vx1y1)
+
